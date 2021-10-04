34f0789126f66f90cd4d1b286a6142b86e2c73c5
Pablo Padilla <Padilla.Pablo1998@Gmail.com>
2021-10-04 14:07:30 -0700

    added ability to add notes

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,13 +1,114 @@
-import React from "react";
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useCallback } from "react";
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, ScrollView } from 'react-native';
+import Note from "./Notes";
 
 const Main = (props) => {
 
+    const [notes, setNotes] = useState([]);
+    const [inputVal, setInputVal] = useState("");
+
+    const addNote = useCallback(() => {
+        if (inputVal.length) {
+            const d = new Date();
+            const payload = {
+                date: `${d.getMonth()}/${d.getDate()}/${d.getFullYear()}`,
+                note: inputVal
+            };
+            setNotes([payload, ...notes]);
+            setInputVal("");
+        }
+    },[notes, inputVal])
+
+
     return (
-        <View>
-            <Text>Test</Text>
+        <View style={ styles.container }>
+            <View style={ styles.header }>
+                <Text style={ styles.headerText }>- RAD to Do's -</Text>
+            </View>
+            <ScrollView style={ styles.scrollContainer }>
+                {
+
+                    notes.map((item, i) => (
+                        <Note key={i} data={item}/>
+                    ))
+                    
+                }
+            </ScrollView>
+            <View style={ styles.footer }>
+                <TextInput
+                value={inputVal}
+                onChangeText={ (userInput) => setInputVal(userInput) }
+                style={ styles.textInput }
+                placeholder="> add something rad..."
+                placeholderTextColor="#eee"
+                underlineColorAndroid="transparent"
+                >
+                </TextInput>
+            </View>
+            <TouchableOpacity onPress={addNote} style={ styles.addButton }>
+                <Text style={ styles.addButtonText }>+</Text>
+            </TouchableOpacity>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    header: {
+        backgroundColor: "#3d3d3d",
+        alignItems: "center",
+        justifyContent: "center",
+        borderBottomWidth: 10,
+        borderBottomColor: "#ddd",
+        paddingTop: 20
+    },
+    headerText: {
+        color: "#FFD700",
+        fontSize: 36,
+        padding: 26,
+        fontWeight: "500",
+    },
+    scrollContainer: {
+        flex: 1,
+        marginBottom: 100,
+    },
+    footer: {
+        position: "absolute",
+        bottom: 0,
+        left: 0,
+        right: 0,
+        zIndex: 10,
+    },
+    textInput: {
+        alignSelf: "stretch",
+        color: "#FFD700",
+        padding: 20,
+        backgroundColor: "#252525",
+        borderTopWidth: 2,
+        borderTopColor: "#ededed",
+        fontSize: 32,
+    },
+    addButton: {
+        position: "absolute",
+        zIndex: 11,
+        right: 20,
+        bottom: 100,
+        backgroundColor: "#3d3d3d",
+        width: 80,
+        height: 80,
+        borderRadius: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        elevation: 8,
+    },
+    addButtonText: {
+        color: "#FFD700",
+        fontSize: 26,
+        fontWeight: "700",
+    },
+
+});
 
 export default Main;
